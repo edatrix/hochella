ea1f559febdd209439773ab34cb76af00e04274a
edatrix <lespath@gmail.com>
2016-04-16 15:10:57 -0600

    puts images on featuring page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,59 @@
 module ApplicationHelper
+
+  WITCHES = [
+    {
+      :name => "Brianne",
+      :image => "brianne.jpg",
+      :about => "",
+      :symbol => "",
+    },
+    {
+      :name => "Catherine",
+      :image => "catherine.jpg",
+      :about => "",
+      :symbol => "",
+    },
+    {
+      :name => "Jessica",
+      :image => "jessica.jpg",
+      :about => "",
+      :symbol => "",
+    },
+    {
+      :name => "Jess",
+      :image => "jss.jpg",
+      :about => "",
+      :symbol => "",
+    },
+    {
+      :name => "Kathleen",
+      :image => "kathleen.jpg",
+      :about => "",
+      :symbol => "",
+    },
+    {
+      :name => "Kay",
+      :image => "kay.jpg",
+      :about => "",
+      :symbol => "",
+    },
+    {
+      :name => "Lauren",
+      :image => "lauren.jpg",
+      :about => "",
+      :symbol => "",
+    },
+    {
+      :name => "Marianne",
+      :image => "marianne.jpg",
+      :about => "",
+      :symbol => "",
+    },
+    {
+      :name => "Meghan",
+      :image => "meghan.jpg",
+      :about => "",
+      :symbol => "",
+    },
+  ]
 end
