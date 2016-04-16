5c6c9b21b78f285b40db9d7426dd5fd005160e92
edatrix <lespath@gmail.com>
2016-04-16 13:12:55 -0600

    adds views for main pages, main font

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,12 @@
 class HomeController < ApplicationController
+
+  def about
+  end
+
+  def lineup
+  end
+
+  def get_ready
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   root :to => 'home#index'
+  get '/about', :to => "home#about"
+  get '/lineup', :to => "home#lineup"
+  get '/get_ready', :to => "home#get_ready"
 end
